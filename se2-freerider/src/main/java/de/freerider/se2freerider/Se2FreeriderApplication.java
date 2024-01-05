4c4193a2943d9c16cef668d2adb19488921d89d5
aurearbor <arthur.goldenbaum@gmail.com>
2024-01-05 13:45:03 +0100

    setup

---
+++ se2-freerider/src/main/java/de/freerider/se2freerider/Se2FreeriderApplication.java
@@ -0,0 +1,13 @@
+package de.freerider.se2freerider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se2FreeriderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se2FreeriderApplication.class, args);
+	}
+
+}
---
+++ se2-freerider/src/test/java/de/freerider/se2freerider/Se2FreeriderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.freerider.se2freerider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2FreeriderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
